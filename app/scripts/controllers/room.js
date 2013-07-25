e7f88275c462587055e7cb7ced6a0654e9c85eba
Bobby Gammill III <bobby@tradeweb.net>
2013-07-25 19:06:51 -0400

    Remove console.log calls and update bower.json

--- app/scripts/controllers/room.js
+++ app/scripts/controllers/room.js
@@ -6,25 +6,10 @@ angular.module('chatApp')
 
   	var url = 'https://notr.firebaseio.com/';
   	var roomUrl = url + "rooms/" + $scope.room.name;
-  	console.log(roomUrl);
 
   	$scope.messages = angularFireCollection(new Firebase(roomUrl).limit(50));
 
-  	// var auth = new FirebaseSimpleLogin(new Firebase(roomUrl), function(error, user) {
-  	// 	if (!error) {
-  	// 		$scope.user = user;
-  	// 		localStorageService.add('firebaseAuthToken', user['firebaseAuthToken']);
-  	// 	} else {
-  	// 		console.log(error);
-  	// 	}
-  	// });
-
-  	// auth.login('password', {
-  	// 	email: 'bgammill@gmail.com',
-  	// 	password: 'test'
-  	// });
-
-	$scope.username = localStorageService.get('username') || localStorageService.add('username', ('Guest' + Math.floor(Math.random()*10001)));
+    $scope.username = localStorageService.get('username') || localStorageService.add('username', ('Guest' + Math.floor(Math.random()*10001)));
 
   	$scope.addMessage = function() {
   		$scope.messages.add({user: $scope.user || $scope.username, content: $scope.message});
